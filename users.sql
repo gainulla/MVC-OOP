78c352c6058a576d7c2c41df0a7e95aedb38edb4
Gainulla <yusupovgz@yandex.ru>
2022-03-25 17:42:10 +0500

    Updated users.sql

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Мар 20 2022 г., 13:44
+-- Время создания: Мар 25 2022 г., 13:41
 -- Версия сервера: 10.4.18-MariaDB
 -- Версия PHP: 7.4.16
 
@@ -29,23 +29,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(255) DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `passwordHash` varchar(255) NOT NULL
+  `passwordHash` varchar(255) NOT NULL,
+  `passwordResetHash` varchar(64) DEFAULT NULL,
+  `passwordResetExpiresAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `passwordHash`) VALUES
-(1, 'dale', 'dale@mail.io', '$2y$10$ttHg6eFDOD6up5aP/ERXmeF4LkSJjZhoWrn9tP2T97QgxagEEQBFS'),
-(2, 'kate', 'kate@mail.io', '$2y$10$7HwCvZcnOSPynfStuaLU8uqBDwxjQoyOsWWTFZCrTUejeWSom0Vr6'),
-(3, 'nick', 'nick@mail.io', '$2y$10$Wz5mwNcH68AoqgZ0iNcmS.EoIacblND9X/BMhwnEecPOwzNrcA/OC'),
-(4, 'jane', 'jane@mail.io', '$2y$10$VBvnwT2gHbdwudziCYELwuKEs1CWm/t6oxT76VvvH.V0ZIl.48jzK'),
-(7, 'anna', 'anna@mail.io', '$2y$10$HudiQRmzgufCl3BM.IiWEeiEu7dVKNsR2a5wr1DZ2nQOr/MjpLe3W'),
-(8, 'gram', 'gram@mail.io', '$2y$10$.WCjW/ImYYSrrb0Zh.VgE.pu1VRNFS/sNDVdcelFg71uj7t4cV0ti'),
-(9, 'lora', 'lora@mail.io', '$2y$10$HpKJMb6XVWXPeTyK/5CilO5LWQv2lB31hU8ujs3vp9ypPwlEm0U3a');
+INSERT INTO `users` (`id`, `username`, `email`, `passwordHash`, `passwordResetHash`, `passwordResetExpiresAt`) VALUES
+(1, 'dale', 'dale@mail.io', '$2y$10$.0Z12cjwZFvJ21Hlymvm0.i34fZjmcKB0Z6g6tQv3ZKAD67zdEM7e', '92aedee7e3df62d41e54b9c18cb971d1a81afcee21debbf1e52291804e976124', '2022-03-25 14:25:44'),
+(2, 'kate', 'kate@mail.io', '$2y$10$t2eixxylA1EHxusCY6QEJ.9ltb746MdL9aDVllL6B/BK3BSHF7uQG', NULL, NULL);
 
 --
 -- Индексы сохранённых таблиц
@@ -66,7 +63,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
