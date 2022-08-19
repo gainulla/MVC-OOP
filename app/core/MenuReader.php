4444175a7479dc8d67f57b1a77628593b914fa83
gainulla <yusupovgz@yandex.ru>
2022-08-19 22:55:30 +0500

    Styles updated

--- app/core/MenuReader.php
+++ app/core/MenuReader.php
@@ -9,11 +9,10 @@ class MenuReader
     public function read(UrlManager $url, bool $isLoggedIn)
     {
         $menu = [];
-        $menu[] = ['url' => $url->for('home/'), 'text' => 'Домашняя'];
+        //$menu[] = ['url' => $url->for('/'), 'text' => ''];
 
         if (!$isLoggedIn) {
-            $menu[] = ['url' => $url->for('auth/register'), 'text' => 'Регистрация'];
-            $menu[] =['url' => $url->for('auth/login'), 'text' => 'Вход'];
+            $menu[] =['url' => $url->for('auth/login'), 'text' => 'Логин'];
         } else {
             $menu[] =['url' => $url->for('auth/logout'), 'text' => 'Выход'];
         }
